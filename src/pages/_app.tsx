be7b4e3464a02678b328a06822dfeb82445a4cc6
Andrew Dobson <tandrewlopez@gmail.com>
2023-03-13 15:41:28 -0400

    installed recoil for state management

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import Layout from "@/components/Layout/Layout";
-import Modal from "@/components/Modal/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Modal title="Test Modal" actionLabel="Submit" isOpen />
       <Layout>
         <Component {...pageProps} />
       </Layout>
