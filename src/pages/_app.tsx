dcd9116ad3fcedf8b38eb871a6b46e79a72a6160
Andrew Dobson <tandrewlopez@gmail.com>
2023-03-25 04:56:19 -0400

    working on custom hooks

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,9 @@ import RegisterModal from "@/components/Modal/RegisterModal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-
-import { Toaster } from "react-hot-toast";
-import { SessionProvider } from "next-auth/react";
 import EditModal from "@/components/Modal/EditModal";
+import { SessionProvider } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
